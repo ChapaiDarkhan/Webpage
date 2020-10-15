2d7d8af05ad615f8ebeb61e744b3f9534f8d682d
darkhan <you@example.com>
2020-10-15 12:47:30 +0600

    [C#-project] Create web page with C# and use sql database.

---
+++ Test/Test/WebForm1.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Test.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 33px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:GridView ID="GridView1" runat="server" Height="426px" style="margin-left: 281px; margin-top: 7px;" Width="807px"></asp:GridView>
+            
+            <br />
+            <table>
+                <tr>
+                    <td>
+                        <asp:TextBox ID="message" runat="server" style="margin-left: 278px" Width="662px" placeholder="Введите сообщение"></asp:TextBox>
+                        <asp:Button ID="Button1" runat="server" Text="Добавить" OnClick="Button1_Click" Width="131px" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Test/Test/WebForm1.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Test
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True;Connect Timeout=30");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+
+            disp_data();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into [Message] values(N'" + message.Text +"')";
+            cmd.ExecuteNonQuery();
+
+            message.Text = "";
+
+            disp_data();
+        }
+
+        public void disp_data()
+        {
+            SqlCommand cm = con.CreateCommand();
+            cm.CommandType = CommandType.Text;
+            cm.CommandText = "select (N'" message "') from [Message]";
+            cm.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cm);
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+        }
+    }
+
+}
